217d37dd4a69e3c566ad12ad1cfa0c16b34eb355
taisei-akahira <kyyhtm1998@gmail.com>
2025-07-27 14:52:30 +0900

    input

--- src/main.py
+++ src/main.py
@@ -8,9 +8,8 @@ def main():
     with open("input/PLAYS.json", "r", encoding="utf-8") as f:
         plays = json.load(f)
 
-    text = "これはテスト出力です。"
-    with open("output.txt", "w", encoding="utf-8") as f:
-      f.write(text)
+    with open("./output/output.txt", "w", encoding="utf-8") as f:
+      f.write(str(invoices))     
     print("output.txt にテキストを書き込みました。")
 
 if __name__ == "__main__":
