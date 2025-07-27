226d0af34f203752ebb0cc28fbb1875ed8fe8cd5
ukekura <ukekura.takuma@gmail.com>
2025-07-27 14:37:36 +0900

    jsonファイル読み込み

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ import os
 
 
 def main():
+    with open("input/INVOICES.json", "r", encoding="utf-8") as f:
+        invoices = json.load(f)
+    with open("input/PLAYS.json", "r", encoding="utf-8") as f:
+        plays = json.load(f)
+
     text = "これはテスト出力です。"
     with open("output.txt", "w", encoding="utf-8") as f:
       f.write(text)
